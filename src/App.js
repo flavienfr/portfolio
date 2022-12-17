deb5f67a5ab787c09219517464893869ed47c1ea
flavienfr <flav.rsl@hotmail.fr>
2022-12-17 19:38:58 +0100

    falling page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,117 @@
-import logo from './logo.svg';
-import './App.css';
+import { Html, useHelper } from '@react-three/drei'
+import { useLoader, useThree } from '@react-three/fiber'
+import { CuboidCollider, Debug, Physics, RigidBody } from '@react-three/rapier'
+import { Suspense, useEffect, useRef, useState } from 'react'
+import { DirectionalLightHelper } from 'three'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { FOV } from '.'
 
-function App() {
+export default function App() {
+  /*   const directionalLight = useRef(null)
+  useHelper(directionalLight, DirectionalLightHelper, 1, 'red')
+  const directionalLight2 = useRef(null)
+  useHelper(directionalLight2, DirectionalLightHelper, 1, 'blue')
+ */
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Physics>
+        <Debug />
+        <WebPage />
+        {/* <NeonModel /> */}
+        <Floor />
+        {/*         <directionalLight
+          ref={directionalLight}
+          position={[20, 20, -20]}
+          color="red"
+          intensity={0.1}
+        />
+        <directionalLight
+          ref={directionalLight2}
+          position={[15, 20, -20]}
+          color="blue"
+          intensity={0.1}
+        /> */}
+      </Physics>
+    </>
+  )
 }
 
-export default App;
+/* function LampModel() {
+  const { nodes, materials } = useLoader(GLTFLoader, '/model/neon_1/scene.gltf')
+  console.log('🚀 ~ file: App.js:23 ~ LampModel ~ materials', materials)
+
+  return (
+    <mesh geometry={nodes.Object_4.geometry} material={materials['LichtFront']}>
+
+    </mesh>
+  )
+} */
+
+function NeonModel() {
+  const gltf = useLoader(GLTFLoader, '/model/neon_1/scene.gltf')
+  console.log('🚀 ~ file: App.js:34 ~ NeonModel ~ gltf', gltf)
+  return (
+    <Suspense fallback={null}>
+      <primitive object={gltf.scene} />
+    </Suspense>
+  )
+}
+
+function WebPage() {
+  const webScreen = useRef()
+  const { camera, viewport } = useThree()
+  const [planeInfo, setPlaneInfo] = useState({ width: 1, height: 1 })
+
+  useEffect(() => {
+    const cameraZ = camera.position.z
+    const planeZ = 682
+    const distance = Math.abs(cameraZ - planeZ)
+    const aspect = viewport.width / viewport.height
+    const vFov = (FOV * Math.PI) / 180
+    const height = 2 * Math.tan(vFov / 2) * distance
+    const width = height * aspect
+    setPlaneInfo({ height, width })
+  }, [setPlaneInfo, viewport, camera])
+
+  useEffect(() => {
+    setTimeout(() => {
+      webScreen.current.applyImpulse({ x: 0, y: 0, z: -10 })
+    }, 1000)
+  }, [webScreen])
+
+  /*   useFrame((state, delta) => {
+    webScreen.current.position.z -= delta
+  }) */
+
+  return (
+    <RigidBody colliders={false} ref={webScreen}>
+      <CuboidCollider args={[0.5, 1.5, 0.5]} position={[0, -2, 0]} />
+
+      <mesh name="Screen" position={[0, 0, -0.5]} /* ref={webScreen} */>
+        <Html
+          /*  occlude */
+          transform={true}
+          wrapperClass="htmlScreen"
+          distanceFactor={3.25}
+          style={{
+            width: planeInfo.width,
+            height: planeInfo.height,
+          }}
+        >
+          <iframe title="myFrame" src="https://bruno-simon.com/html/" />
+        </Html>
+      </mesh>
+    </RigidBody>
+  )
+}
+
+function Floor() {
+  return (
+    <RigidBody type="fixed">
+      <mesh rotation={[-Math.PI * 0.5, 0, 0]} position={[0, -4, 0]}>
+        <boxGeometry args={[25, 25, 1]} />
+        <meshNormalMaterial attach="material" />
+      </mesh>
+    </RigidBody>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { OrbitControls } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import reportWebVitals from './reportWebVitals'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.querySelector('#root'))
+export const FOV = 75
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Canvas /* camera={{ position: [0, 0, 0] }} */>
+      <color args={['white']} attach="background" />
+      <OrbitControls makeDefault />
+      <App />
+    </Canvas>
   </React.StrictMode>
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
